7971f2896a59320122a978475ef1fd6fdb44b459
RonenTGreat <ronenh53@gmail.com>
2025-11-16 22:59:44 +0000

    .

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,11 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- Vite compiled CSS & JS -->
-    @vite([
-        'resources/css/app.css',
-        'resources/css/style.css',
-        'resources/js/app.js'
-    ])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <!-- AlpineJS -->
     <script src="//unpkg.com/alpinejs" defer></script>
